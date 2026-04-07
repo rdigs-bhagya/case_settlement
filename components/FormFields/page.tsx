f5feae869b915e2e5d99676d96291946acb99542
bhagyashree patil <115216811+BHAGU2001@users.noreply.github.com>
2026-04-07 22:06:11 +0530

    removed it

--- components/FormFields/page.tsx
+++ components/FormFields/page.tsx
@@ -203,7 +203,7 @@ I understand that my wireless carrier may charge me for such communications and 
           onChange={handleChange}
           className={inputStyles}
         >
-          <option value="" disabled hidden>Select Your Case Type</option>
+          <option value="">Select Your Case Type</option>
           {[
             "AFFF Lawsuit Support",
             "NEC Lawsuit Assistance",
