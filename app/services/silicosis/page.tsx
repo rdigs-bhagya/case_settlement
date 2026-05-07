3e79a8cb7a0d51bb83abfeb261860e64bff8523c
bhagyashree patil <115216811+BHAGU2001@users.noreply.github.com>
2026-05-07 22:11:18 +0530

    silicosis first image added

--- app/services/silicosis/page.tsx
+++ app/services/silicosis/page.tsx
@@ -14,7 +14,7 @@ const SilicosisSection = () => {
                     <div className="flex flex-wrap items-start gap-10">
                         <div className="w-full md:w-1/3 mt-16">
                             <Image
-                                src="\silicosis\Silicosis.png"
+                                src="/silicosis/Silicosis (3).png"
                                 alt="Silicosis lung disease illustration"
                                 width={500}
                                 height={300}
