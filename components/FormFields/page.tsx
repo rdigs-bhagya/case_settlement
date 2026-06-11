ae295a2dfcb91a5254a7fa436f35d685d84be57d
bhagyashree patil <115216811+BHAGU2001@users.noreply.github.com>
2026-06-11 22:00:36 +0530

    ridehsare added

--- components/FormFields/page.tsx
+++ components/FormFields/page.tsx
@@ -220,7 +220,7 @@ I understand that my wireless carrier may charge me for such communications and 
             "Depo-Provera",
             "Social Media Addiction",
             "Talcum Powder",
-            "Rideshare"
+            "Rideshare",
             "Other"
           ].map((caseType) => (
             <option key={caseType} value={caseType}>
