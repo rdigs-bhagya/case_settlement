8ad5752148edf6c86a32bcacc9d4ebf3a7382d48
bhagyashree patil <115216811+BHAGU2001@users.noreply.github.com>
2026-07-24 21:53:29 +0530

    api changed new api

--- components/FormFields/page.tsx
+++ components/FormFields/page.tsx
@@ -107,7 +107,7 @@ I understand that my wireless carrier may charge me for such communications and 
     };
 
     try {
-      const response = await fetch("https://g8l8qu2ccf.execute-api.us-east-1.amazonaws.com/dev/contact", {
+      const response = await fetch("https://wcyf5iypbi.execute-api.us-east-1.amazonaws.com/dev/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(userPayload),
